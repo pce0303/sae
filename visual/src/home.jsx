61e5f2135d0c315f3fd2dfd8ce369c58e1adfd67
pce0303 <ivychoen@gmail.com>
2024-12-04 15:30:45 +0900

    asdf

--- visual/src/home.jsx
+++ visual/src/home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import './style.css';  // Import the CSS file
+import './style.css';
 
 const Home = () => {
   return (
@@ -8,11 +8,11 @@ const Home = () => {
       <Link to="https://dent-cattle-c60.notion.site/WPA2-WPA3-SAE-1507f0e96a0c8073981bd83b62108692" target="_blank">
         <button className="button">이론 보러가기</button>
       </Link>
-      <Link to="/sae">
-        <button className="button">SAE 시뮬레이터</button>
-      </Link>
       <Link to="/psk">
         <button className="button">Dictionary Attack 실습</button>
+      </Link>
+      <Link to="/sae">
+        <button className="button">SAE 시뮬레이터</button>
       </Link>
     </div>
   );
